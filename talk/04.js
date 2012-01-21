6ebde07d33fde8aa4849a64a218b3660502507dc
Robert Davis <rtdavis22@gmail.com>
2012-01-21 00:53:40 -0500

    Fixed typo in sample.

--- talk/04.js
+++ talk/04.js
@@ -3,10 +3,10 @@ load("glycam.js");
 // For reference:
 // "DGalpNAcb1-4DGlcpNAcb1-2DManpa1-6[DGlcpNAcb1-4[DGalpNAcb1-4DGlcpNAcb1-2]DManpa1-3]DManpb1-4DGlcpNAcb1-4DGlcpNAcb1-OH"
 
-// This is a more intuitive way to represent structures. The structures is
+// This is a more intuitive way to represent structures. The structure is
 // represented as a sequence of substructures. Branches are denoted by
-// brackets: open brackets start branches and close brackets end branches
-// (similar to the above sequence). Substructures are represented as
+// brackets (arrays): left brackets start branches and right brackets end
+// branches (similar to the above sequence). Substructures are represented as
 // JSON objects (a set of key-value pairs). A string like "ROH" is shorthand
 // for { code: "ROH" }. The "at" property specifies where this substructure
 // attaches to its parent substructure. If not specified, the parent's tail
